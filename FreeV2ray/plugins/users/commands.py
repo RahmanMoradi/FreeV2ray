3baecc210f4e249873a918d50356177651e9c6bc
loeloveroob <loeloveroob@gmail.com>
2024-01-07 09:57:23 -0500

    debug

--- FreeV2ray/plugins/users/commands.py
+++ FreeV2ray/plugins/users/commands.py
@@ -66,9 +66,9 @@ async def get_config_handler(client: Client, message: Message):
     v2ray_config, created = panel.get_or_create_client(str(message.from_user.id))
     if created:
         # TODO: add the config that has been created to the database
-        await message.reply(v2ray_config)
+        await start_handler(client, message, v2ray_config)
     else:
-        await message.reply("شما در حال حاضر کانفیگ دارین!")
+        await start_handler(client, message, "شما در حال حاضر کانفیگ دارین!")
 
 
 async def my_config_handler(client: Client, message: Message):
