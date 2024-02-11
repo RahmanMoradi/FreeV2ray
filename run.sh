0705c4c8fd1c842bfce7c552b9e9e0cd19713099
loeloveroob <loeloveroob@gmail.com>
2024-02-11 04:11:41 -0500

    better construction

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 # this file must not be run from parent or another directory!
 # just cd into this file directory and then: sh run.sh or ./run.sh
 
-python3 -m venv venv
+python3.12 -m venv venv
 
 ./venv/bin/pip install -r requirements.txt
 ./venv/bin/pip install -e . --upgrade
 
-./venv/bin/python3 FreeV2ray/app.py
+./venv/bin/python3.12 FreeV2ray/app.py
